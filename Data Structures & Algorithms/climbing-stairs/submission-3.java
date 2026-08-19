8e099c94ea19b68e97a4d6de43b040d8d62395fe
Tousif Rahaman <tousifrahamanwork12@gmail.com>
2026-08-19 12:08:29 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n);
+    }
+    int solve(int n){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = solve(n-1) + solve(n-2);
+        return dp[n];
+    }
+}
